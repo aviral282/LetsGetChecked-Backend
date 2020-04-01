a807ae10be8ccbf63fd7441177e862dde8e9f96a
Aviral Rajput <aviral282@gmail.com>
2020-04-01 20:15:04 +0100

    chore: test failure

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -29,6 +29,11 @@ export class PostsController {
         return this._postsService.addCommentOnPost(id, comment);
     }
 
+    @Post('posts/:id/comments')
+    async addComment(@Param('id') id, @Body() comment: Comments) {
+        return this._postsService.addCommentOnPost(id, comment);
+    }
+
     @Put('comments/:id')
     async updateComment(@Param('id') toChangeID, @Body() comment: Comments) {
         return this._postsService.updateCommentOfId(toChangeID, comment);

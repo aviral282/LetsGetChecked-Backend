43b1930fd4ff5bc03c1ea86ba12346ad354a3eda
Aviral Rajput <aviral282@gmail.com>
2020-04-04 12:07:30 +0100

    chore: updated server port for heroku

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule, { cors: true });
-    await app.listen(process.env.PORT || 3000);
-    console.log('Server created on http://localhost:3000/');
+  const app = await NestFactory.create(AppModule, { cors: true });
+  await app.listen(process.env.PORT || 3000);
+  console.log("Server created on http://localhost:" + (process.env.PORT || 3000) + "/");
 }
 
 bootstrap();
